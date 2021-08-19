166103611d3e492f4a3bc305ae1c3083890b4af1
martilux2580 <64471789+martilux2580@users.noreply.github.com>
2021-08-19 21:11:41 +0200

    Appearance improved. Minor functionalities added.

--- PassGuard/GUI/Form1.Designer.cs
+++ PassGuard/GUI/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace PassGuard
 {
-    partial class Form1
+    partial class mainWindow
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -29,22 +29,206 @@ namespace PassGuard
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.LeftPanel = new System.Windows.Forms.Panel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.MenuPanel = new System.Windows.Forms.Panel();
+            this.LoadVaultButton = new System.Windows.Forms.Button();
+            this.CreateVaultButton = new System.Windows.Forms.Button();
+            this.LogoPanel = new System.Windows.Forms.Panel();
+            this.LogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.DesignerLabel = new System.Windows.Forms.Label();
+            this.LogoLabel = new System.Windows.Forms.Label();
+            this.OptionsPanel = new System.Windows.Forms.Panel();
+            this.ToolTipNewPassVault = new System.Windows.Forms.ToolTip(this.components);
+            this.ToolTipLoadPassVault = new System.Windows.Forms.ToolTip(this.components);
+            this.LeftPanel.SuspendLayout();
+            this.MenuPanel.SuspendLayout();
+            this.LogoPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // LeftPanel
+            // 
+            this.LeftPanel.Controls.Add(this.treeView1);
+            this.LeftPanel.Location = new System.Drawing.Point(1041, 338);
+            this.LeftPanel.Name = "LeftPanel";
+            this.LeftPanel.Size = new System.Drawing.Size(391, 348);
+            this.LeftPanel.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.Menu;
+            this.treeView1.Location = new System.Drawing.Point(4, 182);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(384, 489);
+            this.treeView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(405, 544);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // MenuPanel
+            // 
+            this.MenuPanel.Controls.Add(this.LoadVaultButton);
+            this.MenuPanel.Controls.Add(this.CreateVaultButton);
+            this.MenuPanel.Controls.Add(this.LogoPanel);
+            this.MenuPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.MenuPanel.Name = "MenuPanel";
+            this.MenuPanel.Size = new System.Drawing.Size(384, 698);
+            this.MenuPanel.TabIndex = 2;
+            // 
+            // LoadVaultButton
+            // 
+            this.LoadVaultButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LoadVaultButton.FlatAppearance.BorderSize = 0;
+            this.LoadVaultButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoadVaultButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadVaultButton.ForeColor = System.Drawing.Color.Black;
+            this.LoadVaultButton.Location = new System.Drawing.Point(38, 270);
+            this.LoadVaultButton.Name = "LoadVaultButton";
+            this.LoadVaultButton.Size = new System.Drawing.Size(309, 59);
+            this.LoadVaultButton.TabIndex = 5;
+            this.LoadVaultButton.Text = "Load Password Vault";
+            this.ToolTipNewPassVault.SetToolTip(this.LoadVaultButton, "Load passwords from a Password Vault.");
+            this.LoadVaultButton.UseVisualStyleBackColor = true;
+            this.LoadVaultButton.Click += new System.EventHandler(this.LoadVaultButton_Click);
+            this.LoadVaultButton.MouseEnter += new System.EventHandler(this.LoadVaultButton_MouseEnter);
+            this.LoadVaultButton.MouseLeave += new System.EventHandler(this.LoadVaultButton_MouseLeave);
+            // 
+            // CreateVaultButton
+            // 
+            this.CreateVaultButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CreateVaultButton.FlatAppearance.BorderSize = 0;
+            this.CreateVaultButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CreateVaultButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateVaultButton.ForeColor = System.Drawing.Color.Black;
+            this.CreateVaultButton.Location = new System.Drawing.Point(37, 193);
+            this.CreateVaultButton.Name = "CreateVaultButton";
+            this.CreateVaultButton.Size = new System.Drawing.Size(309, 59);
+            this.CreateVaultButton.TabIndex = 4;
+            this.CreateVaultButton.Text = "New Password Vault";
+            this.ToolTipNewPassVault.SetToolTip(this.CreateVaultButton, "Create a Safe Vault to Store your Passwords");
+            this.CreateVaultButton.UseVisualStyleBackColor = true;
+            this.CreateVaultButton.Click += new System.EventHandler(this.CreateVaultButton_Click);
+            this.CreateVaultButton.MouseEnter += new System.EventHandler(this.CreateVaultButton_MouseEnter);
+            this.CreateVaultButton.MouseLeave += new System.EventHandler(this.CreateVaultButton_MouseLeave);
+            // 
+            // LogoPanel
+            // 
+            this.LogoPanel.Controls.Add(this.LogoPictureBox);
+            this.LogoPanel.Controls.Add(this.DesignerLabel);
+            this.LogoPanel.Controls.Add(this.LogoLabel);
+            this.LogoPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.LogoPanel.Location = new System.Drawing.Point(0, 0);
+            this.LogoPanel.Name = "LogoPanel";
+            this.LogoPanel.Size = new System.Drawing.Size(384, 164);
+            this.LogoPanel.TabIndex = 3;
+            // 
+            // LogoPictureBox
+            // 
+            this.LogoPictureBox.Location = new System.Drawing.Point(128, 3);
+            this.LogoPictureBox.Name = "LogoPictureBox";
+            this.LogoPictureBox.Size = new System.Drawing.Size(126, 103);
+            this.LogoPictureBox.TabIndex = 3;
+            this.LogoPictureBox.TabStop = false;
+            // 
+            // DesignerLabel
+            // 
+            this.DesignerLabel.AutoSize = true;
+            this.DesignerLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DesignerLabel.Font = new System.Drawing.Font("Mongolian Baiti", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DesignerLabel.ForeColor = System.Drawing.Color.Black;
+            this.DesignerLabel.Location = new System.Drawing.Point(91, 138);
+            this.DesignerLabel.Name = "DesignerLabel";
+            this.DesignerLabel.Size = new System.Drawing.Size(197, 18);
+            this.DesignerLabel.TabIndex = 1;
+            this.DesignerLabel.Text = "Designed by martilux2580";
+            this.DesignerLabel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.DesignerLabel_MouseClick);
+            this.DesignerLabel.MouseEnter += new System.EventHandler(this.DesignerLabel_MouseEnter);
+            this.DesignerLabel.MouseLeave += new System.EventHandler(this.DesignerLabel_MouseLeave);
+            // 
+            // LogoLabel
+            // 
+            this.LogoLabel.AutoSize = true;
+            this.LogoLabel.Font = new System.Drawing.Font("Modern No. 20", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LogoLabel.ForeColor = System.Drawing.Color.Black;
+            this.LogoLabel.Location = new System.Drawing.Point(111, 104);
+            this.LogoLabel.Name = "LogoLabel";
+            this.LogoLabel.Size = new System.Drawing.Size(158, 34);
+            this.LogoLabel.TabIndex = 0;
+            this.LogoLabel.Text = "PassGuard";
+            // 
+            // OptionsPanel
+            // 
+            this.OptionsPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.OptionsPanel.Location = new System.Drawing.Point(384, 0);
+            this.OptionsPanel.Name = "OptionsPanel";
+            this.OptionsPanel.Size = new System.Drawing.Size(1195, 164);
+            this.OptionsPanel.TabIndex = 3;
+            // 
+            // ToolTipNewPassVault
+            // 
+            this.ToolTipNewPassVault.AutomaticDelay = 300;
+            this.ToolTipNewPassVault.AutoPopDelay = 5000;
+            this.ToolTipNewPassVault.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ToolTipNewPassVault.InitialDelay = 300;
+            this.ToolTipNewPassVault.ReshowDelay = 60;
+            // 
+            // ToolTipLoadPassVault
+            // 
+            this.ToolTipLoadPassVault.AutomaticDelay = 300;
+            this.ToolTipLoadPassVault.AutoPopDelay = 5000;
+            this.ToolTipLoadPassVault.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ToolTipLoadPassVault.InitialDelay = 300;
+            this.ToolTipLoadPassVault.ReshowDelay = 60;
+            // 
+            // mainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1579, 698);
-            this.Name = "Form1";
+            this.Controls.Add(this.OptionsPanel);
+            this.Controls.Add(this.MenuPanel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LeftPanel);
+            this.Name = "mainWindow";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "PassGuard™";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.LeftPanel.ResumeLayout(false);
+            this.MenuPanel.ResumeLayout(false);
+            this.LogoPanel.ResumeLayout(false);
+            this.LogoPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel LeftPanel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Panel MenuPanel;
+        private System.Windows.Forms.Panel LogoPanel;
+        private System.Windows.Forms.Label LogoLabel;
+        private System.Windows.Forms.Label DesignerLabel;
+        private System.Windows.Forms.PictureBox LogoPictureBox;
+        private System.Windows.Forms.Panel OptionsPanel;
+        private System.Windows.Forms.Button CreateVaultButton;
+        private System.Windows.Forms.ToolTip ToolTipNewPassVault;
+        private System.Windows.Forms.Button LoadVaultButton;
+        private System.Windows.Forms.ToolTip ToolTipLoadPassVault;
     }
 }
 
--- PassGuard/GUI/Form1.cs
+++ PassGuard/GUI/Form1.cs
@@ -7,19 +7,90 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PassGuard
 {
-    public partial class Form1 : Form
+    public partial class mainWindow : Form
     {
-        public Form1()
+        public mainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            MenuPanel.BackColor = Color.FromArgb(5, 196, 149); //43, 43, 43
+            LogoPanel.BackColor = Color.FromArgb(0, 184, 137); //31, 31, 31
+            OptionsPanel.BackColor = Color.FromArgb(0, 191, 144); //38, 38, 38
+            this.BackColor = Color.FromArgb(225, 225, 225); //45, 45, 45
+            var directorio = Directory.GetCurrentDirectory();
+            label1.Text = directorio;
+            label1.Visible = false;
+            LogoPictureBox.Image = Image.FromFile(@"..\..\Images\Logo.png"); //Working Directory inside Release Folder. Loads Image from Image folder.
+            LogoPictureBox.SizeMode = PictureBoxSizeMode.StretchImage; //Makes the image fit into PictureBox by resizing it.
+            this.Icon = new Icon(@"..\..\Images\LogoIcon64.ico"); //Loads Icon from Image folder.
+            //Determine buttons and functions
+            //Think of use cases of the app.
+            //Cover all panels with the contents they will have before start coding functionality.
+            //When changing window colors, disable black tones (0, 0, 0) so that letters and logo are visible.
 
+            //label2.ForeColor = Color.FromArgb(0, 0, 0); //100, 100, 100
+            //label3.ForeColor = Color.FromArgb(0, 0, 0); //90, 90, 90
+            //CreateVaultButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            //CreateVaultButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            //button1.Visible = false; //If button not visible, you cannot click it
+            //LogoPictureBox.Image = null; //Remove Image from PictureBox (without removing PictureBox itself)
+            //LogoPictureBox.BackColor = Color.FromArgb();
+
+
+
+        }
+
+        private void CreateVaultButton_Click(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+        }
+
+        private void CreateVaultButton_MouseEnter(object sender, EventArgs e)
+        {
+            CreateVaultButton.Font = new Font("Microsoft Sans Serif", 14, FontStyle.Underline);
+        }
+
+        private void CreateVaultButton_MouseLeave(object sender, EventArgs e)
+        {
+            CreateVaultButton.Font = new Font("Microsoft Sans Serif", 14, FontStyle.Regular);
+        }
+
+        private void LoadVaultButton_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+        }
+
+        private void LoadVaultButton_MouseEnter(object sender, EventArgs e)
+        {
+            LoadVaultButton.Font = new Font("Microsoft Sans Serif", 14, FontStyle.Underline);
+        }
+
+        private void LoadVaultButton_MouseLeave(object sender, EventArgs e)
+        {
+            LoadVaultButton.Font = new Font("Microsoft Sans Serif", 14, FontStyle.Regular);
+        }
+
+        private void DesignerLabel_MouseEnter(object sender, EventArgs e)
+        {
+            DesignerLabel.Font = new Font("Mongolian Baiti", 10, FontStyle.Underline);
+        }
+
+        private void DesignerLabel_MouseLeave(object sender, EventArgs e)
+        {
+            DesignerLabel.Font = new Font("Mongolian Baiti", 10, FontStyle.Regular);
+        }
+
+        private void DesignerLabel_MouseClick(object sender, MouseEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/martilux2580?tab=repositories");
         }
     }
 }
